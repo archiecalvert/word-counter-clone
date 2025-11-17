c93a9a33d2fabd5d1d8508f26d4571769b82e4e5
Archie Calvert <archie.calvert@student.manchester.ac.uk>
2025-11-17 22:44:39 +0000

    added documentation for functions:

--- wc.py
+++ wc.py
@@ -1,7 +1,16 @@
 import sys
 
 
-def get_sys_args():
+def get_sys_args() -> list[str]:
+    """
+    Gets a list of all of the arguments passed in on the terminal by the user.
+
+    Returns:
+        list[str]: A list of all the arguments passed in by the user.
+
+    Throws:
+        Exception: No arguments passed.
+    """
     arguments = sys.argv
 
     argument_count = len(arguments) - 1
@@ -12,7 +21,20 @@ def get_sys_args():
     return arguments[1:]
 
 
-def read_file_data(path):
+def read_file_data(path) -> list[bytes]:
+    """
+    Reads the raw byte data from the secified path.
+
+    Parameters:
+        path (str): the filepath where the data will be read from.
+
+    Returns:
+        list[byte]: an array of byte data from the file.
+
+    Throws:
+        FileNotFoundError: if the specified file cannot be found.
+
+    """
     try:
         with open(path, "rb") as f:
             data = f.read()
@@ -22,18 +44,39 @@ def read_file_data(path):
         raise Exception("File does not exist.")
 
 
-def read_byte_count_from_file(path):
+def read_byte_count_from_file(path) -> int:
+    """
+    Gets the number of bytes inside of a file.
+
+    Parameters:
+        path (str): the filepath for the data.
+
+    Returns:
+        int: the number of bytes in the specified file.
+    """
     return len(read_file_data(path))
 
 
-def read_word_count_from_file(path):
+def read_word_count_from_file(path) -> int:
+    """
+    Gets the number of words inside of a file.
+    A word is any length of characters which are non-whitespace characters.
+    A contiguous string of whitespace characters will not count towards the character count.
+
+    Parameters:
+        path (str): the filepath for the data.
+
+    Returns:
+        int: the number of words in a file.
+    """
     data = read_file_data(path)
 
     word_count = 0
     is_word = False
 
-    for b in data:
-        if b in (9, 10, 11, 12, 13, 32):
+    for char in data:
+        # if the current character is whitespace (these are the byte equivalents)
+        if char in (9, 10, 11, 12, 13, 32):
             is_word = False
         else:
             if not is_word:
@@ -43,7 +86,16 @@ def read_word_count_from_file(path):
     return word_count
 
 
-def get_line_count(path):
+def get_line_count(path) -> int:
+    """
+    Gets the number of lines inside of a file, achieved through counting the number of newline characters.
+
+    Parameters:
+        path (str): the filepath for the data.
+
+    Returns:
+        int: the number of lines.
+    """
     data = read_file_data(path)
 
     return data.count(ord("\n"))
